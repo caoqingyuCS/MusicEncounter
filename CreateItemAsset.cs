a6099a12b499752211f29cf66b1b9f3a67fed168
caoqingyuCS <44070729+caoqingyuCS@users.noreply.github.com>
2023-04-29 01:03:02 -0400

    Add files via upload

--- CreateItemAsset.cs
+++ CreateItemAsset.cs
@@ -5,5 +5,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "ItemAsset", menuName = "CreateItemAsset", order = 0)] // 创建assets的特性
 public class CreateItemAsset : ScriptableObject//存放所有的Item数据， scriptable是父类，创建assets
 {
-    public List<ItemData> EquipmentDataLst;
+   public List<EquipmentData> EquipmentDataLst; //普通装备
+   public List<InstrumentData> InstrumentDataLst; //乐器
 }
